ae3e1ea932586b5475b60793b345314067ca326f
Adil <https://_redemption@bitbucket.org/_redemption/lu-canteen.git>
2018-01-15 03:24:36 +0600

    email received from params and link damaging added

--- routes/students.js
+++ routes/students.js
@@ -2,31 +2,65 @@ const router = require('express').Router();
 const passport = require('passport');
 const passportConfig = require('../config/passport');
 const Student = require('../models/students');
+const { ProjectSubmit } = require('../models/proposals');
+var crypto = require('crypto'),
+algorithm = 'aes-256-ctr',
+password = 'd6F3Efeq';
 var proposalID;
+var emailId;
+var decryptedEmail;
+var token;
 
 /* SIGNUP ROUTE */
-router.route('/signup/:id')
+router.route('/signup/:id/:email/:token')
 
   .get((req, res, next) => {
     proposalID = req.params.id;
-    res.render('accounts/signup', { message: req.flash('errors')});
-  })
+    emailId = req.params.email;
+    token = req.params.token;
+    decryptedEmail = decrypt(emailId);
+    console.log('id: ',proposalID);
+    console.log('encrypted email: ',emailId);
+    console.log('email: ',decryptedEmail);
+    console.log('token: ',token);
+
+    function decrypt(text){
+      var decipher = crypto.createDecipher(algorithm,password)
+      var dec = decipher.update(text,'hex','utf8')
+      dec += decipher.final('utf8');
+      return dec;
+    }
+
+    Student.findOne({ secretToken : token }, function(err, user) {
+      
+          
+        if (user) {
+          console.log("Link damaged");
+          return res.render('error');
+          
+        }
+        res.render('accounts/signup', {user:user, message: req.flash('errors')});
+  });
+})
 
   .post((req, res, next) => {
     
     Student.findOne({ email: req.body.email }, function(err, existingUser) {
       if (existingUser) {
         req.flash('errors',  'Account with that email address already exists.');
-        return res.redirect(`/signup/${proposalID}`);
+        return res.redirect(`/signup/${proposalID}/${emailId}`);
       } else {
+
+        
         console.log('id : ',proposalID);
         var student = new Student();
         student.name = req.body.name;
         student.username = req.body.username;
-        student.email = req.body.email;
+        student.email = decryptedEmail;
         student.password = req.body.password;
-
         student.proposal_id = proposalID;
+        student.secretToken = token;
+
         student.save(function(err) {
           if (err) return next(err);
           req.logIn(student, function(err) {
@@ -35,6 +69,8 @@ router.route('/signup/:id')
             res.redirect('/');
           });
         });
+
+
       }
     });
   });
